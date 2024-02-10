e16429cb0c7f1506779eadbd897b10548db260e0
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-10 17:24:15 -0600

    Create signup api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,7 +3,17 @@ import User from '../models/UserModel.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-export const signUp = async (req, res, next) => {};
+export const signUp = async (req, res, next) => {
+      const { username, email, password } = req.body;
+      const hashedPassword = bcryptjs.hashSync(password, 10);
+      const newUser = new User({ username, email, password: hashedPassword });
+      try {
+          await newUser.save();
+          res.status(201).json('User created successfully!');
+      } catch (error) {
+          next(error);
+      }
+};
 
 export const signIn = async (req, res, next) => {};
 
