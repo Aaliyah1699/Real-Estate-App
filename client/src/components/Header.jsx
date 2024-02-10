cc8be714d7fda2869cd382b69ab84f8e2196d626
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-10 15:12:30 -0600

    Create header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,52 @@
-
+import { Link } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { useEffect, useState } from 'react';
+import { BsSearchHeart } from 'react-icons/bs';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <header className='bg-gray-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                {/* Logo */}
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-gray-500'>Realty</span>
+                        <span className='text-gray-700'>Rise</span>
+                    </h1>
+                </Link>
+                {/* Search */}
+                <form className='bg-gray-100 p-3 rounded-lg flex items-center'>
+                    <input
+                        type='text'
+                        placeholder='Search...'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    />
+                    <button>
+                        <BsSearchHeart className='text-gray-600' />
+                    </button>
+                </form>
+                {/* Header Links */}
+                <ul className='flex gap-4'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-gray-700 hover:underline'>
+                            Home
+                        </li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='hidden sm:inline text-gray-700 hover:underline'>
+                            About
+                        </li>
+                    </Link>
+                    <Link to='/profile'>
+                        {/* <img className=''/> */}
+                        <li className='text-gray-700 hover:underline'>
+                            Sign In
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
