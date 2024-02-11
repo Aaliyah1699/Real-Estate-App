4dce70b6c025e8508465fb0a56e02525a2a4202b
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-10 23:25:24 -0600

    Update Header profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { BsSearchHeart } from 'react-icons/bs';
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <header className='bg-gray-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -37,11 +39,20 @@ const Header = () => {
                             About
                         </li>
                     </Link>
+
                     <Link to='/profile'>
-                        {/* <img className=''/> */}
-                        <li className='text-gray-700 hover:underline'>
-                            Sign In
-                        </li>
+                        {currentUser ? (
+                            <img
+                                className='rounded-full h-8 w-8 object-cover'
+                                src={currentUser.avatar}
+                                alt='profile'
+                            />
+                        ) : (
+                            <li className=' text-slate-700 hover:underline'>
+                                {' '}
+                                Sign in
+                            </li>
+                        )}
                     </Link>
                 </ul>
             </div>
