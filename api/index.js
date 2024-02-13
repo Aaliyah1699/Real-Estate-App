fb576ca244a880acd704de6760c387bc30a57571
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-12 20:46:06 -0600

    Set up server

--- api/index.js
+++ api/index.js
@@ -1,47 +1,47 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import path from 'path';
 import userRouter from './routes/userRoutes.js';
 import listingRouter from './routes/listingRoutes.js';
 import authRouter from './routes/authRoutes.js';
+import connectDB from './db/db.js';
 dotenv.config();
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+const __dirname = path.resolve();
 
-// const __dirname = path.resolve();
-
+const port = 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
-
-// app.use((err, req, res, next) => {
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || 'Internal Server Error';
-//     return res.status(statusCode).json({
-//         success: false,
-//         statusCode,
-//         message,
-//     });
-// });
-
-app.listen(5000, () => {
-    console.log('Server is running on port 3000!');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server listening on port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
