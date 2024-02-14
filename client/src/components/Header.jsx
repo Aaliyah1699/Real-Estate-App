d411bb8179cb5cc76399efcfb797bd2900d6e074
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-13 21:13:10 -0600

    Add search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
 import { BsSearchHeart } from 'react-icons/bs';
 
 const Header = () => {
     const { currentUser } = useSelector((state) => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
     return (
         <header className='bg-gray-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -17,10 +34,15 @@ const Header = () => {
                     </h1>
                 </Link>
                 {/* Search */}
-                <form className='bg-gray-100 p-3 rounded-lg flex items-center'>
+                <form
+                    onSubmit={handleSubmit}
+                    className='bg-gray-100 p-3 rounded-lg flex items-center'
+                >
                     <input
                         type='text'
                         placeholder='Search...'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                         className='bg-transparent focus:outline-none w-24 sm:w-64'
                     />
                     <button>
@@ -34,6 +56,7 @@ const Header = () => {
                             Home
                         </li>
                     </Link>
+
                     <Link to='/about'>
                         <li className='hidden sm:inline text-gray-700 hover:underline'>
                             About
