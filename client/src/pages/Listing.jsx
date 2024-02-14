c93a47e081ea2459dba57ddf71c05d583d52d6bb
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-13 19:44:12 -0600

    Create listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,9 +1,175 @@
-
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import {
+    TbBath,
+    TbBed,
+    TbArmchair2,
+    TbMapPin,
+    TbParking,
+    TbShare3,
+} from 'react-icons/tb';
+import Contact from '../components/Contact';
 
 const Listing = () => {
-  return (
-    <div>Listing</div>
-  )
-}
+    SwiperCore.use([Navigation]);
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
+    const params = useParams();
+    const { currentUser } = useSelector((state) => state.user);
 
-export default Listing
+    useEffect(() => {
+        const fetchListing = async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`/api/listing/get/${params.listingId}`);
+                const data = await res.json();
+                if (data.success === false) {
+                    setError(true);
+                    setLoading(false);
+                    return;
+                }
+                setListing(data);
+                setLoading(false);
+                setError(false);
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        };
+        fetchListing();
+    }, [params.listingId]);
+
+    return (
+        <main>
+            {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+            {error && (
+                <p className='text-center my-7 text-2xl'>
+                    Something went wrong!
+                </p>
+            )}
+            {listing && !loading && !error && (
+                <div>
+                    {/* Image */}
+                    <Swiper navigation>
+                        {listing.imageUrls.map((url) => (
+                            <SwiperSlide key={url}>
+                                <div
+                                    className='h-[550px]'
+                                    style={{
+                                        background: `url(${url}) center no-repeat`,
+                                        backgroundSize: 'cover',
+                                    }}
+                                ></div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    {/* Share */}
+                    <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-gray-200 cursor-pointer'>
+                        <TbShare3
+                            className='text-sky-700 text-lg'
+                            onClick={() => {
+                                navigator.clipboard.writeText(
+                                    window.location.href
+                                );
+                                setCopied(true);
+                                setTimeout(() => {
+                                    setCopied(false);
+                                }, 2000);
+                            }}
+                        />
+                    </div>
+                    {/* Copy link */}
+                    {copied && (
+                        <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+                            Link copied!
+                        </p>
+                    )}
+                    {/* Listing info */}
+                    <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+                        <p className='text-2xl font-semibold capitalize'>
+                            {listing.name} - ${' '}
+                            {listing.offer
+                                ? listing.discountPrice.toLocaleString('en-US')
+                                : listing.regularPrice.toLocaleString('en-US')}
+                            {listing.type === 'rent' && ' / month'}
+                        </p>
+                        <p className='flex items-center mt-6 gap-2 text-gray-700  text-lg capitalize'>
+                            <TbMapPin className='text-sky-700' />
+                            {listing.address}
+                        </p>
+                        <div className='flex gap-4'>
+                            <p className='bg-red-800 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                {listing.type === 'rent'
+                                    ? 'For Rent'
+                                    : 'For Sale'}
+                            </p>
+                            {listing.offer && (
+                                <p className='bg-sky-700 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                    $
+                                    {+listing.regularPrice -
+                                        +listing.discountPrice}{' '}
+                                    OFF
+                                </p>
+                            )}
+                        </div>
+                        <p className='text-slate-800'>
+                            <span className='font-semibold text-black'>
+                                Description -{' '}
+                            </span>
+                            {listing.description}
+                        </p>
+                        {/* House info */}
+                        <ul className='text-gray-800 font-semibold flex flex-wrap items-center gap-4 sm:gap-6 text-xl capitalize'>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <TbBed className='text-lg text-sky-700' />
+                                {listing.bedrooms > 1
+                                    ? `${listing.bedrooms} beds `
+                                    : `${listing.bedrooms} bed `}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <TbBath className='text-lg text-sky-700' />
+                                {listing.bathrooms > 1
+                                    ? `${listing.bathrooms} baths `
+                                    : `${listing.bathrooms} bath `}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <TbParking className='text-lg text-sky-700' />
+                                {listing.parking
+                                    ? 'Parking spot'
+                                    : 'No Parking'}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <TbArmchair2 className='text-lg text-sky-700' />
+                                {listing.furnished
+                                    ? 'Furnished'
+                                    : 'Unfurnished'}
+                            </li>
+                        </ul>
+                        {/* Contact */}
+                        {currentUser &&
+                            listing.userRef !== currentUser._id &&
+                            !contact && (
+                                <button
+                                    onClick={() => setContact(true)}
+                                    className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+                                >
+                                    Contact landlord
+                                </button>
+                            )}
+                        {contact && <Contact listing={listing} />}
+                    </div>
+                </div>
+            )}
+        </main>
+    );
+};
+
+export default Listing;
